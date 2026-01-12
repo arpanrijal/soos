47fe15e30789dbbc25d993f3b6f02234731a1422
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-01-13 00:16:33 +0545

    added dynamic env of frontend in vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['soos-o0d5.onrender.com'],
+    allowedHosts: [`${env.VITE_BACKEND_URL}`],
   },
 });
