7a77c636c654aa52547e13c16457774b38bfdb12
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-01-13 01:52:59 +0545

    merging changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-  console.log('Loaded VITE_BACKEND_URL:', env.VITE_BACKEND_URL);
+  
   return defineConfig({
     plugins: [react()],
-    preview: {
-      allowedHosts: [env.VITE_BACKEND_URL]
-    },
+    allowedHosts: [
+      'soos-o0d5.onrender.com',
+      'localhost'
+    ],
   });
 };
