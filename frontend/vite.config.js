2e61e099b7dbdc1f9d281e07758f7e84bf245693
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-01-13 00:46:35 +0545

    added env in vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    allowedHosts: [`${env.VITE_BACKEND_URL}`],
-  },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return defineConfig({
+    plugins: [react()],
+    preview: {
+      allowedHosts: [env.VITE_BACKEND_URL],
+    },
+  });
+};
