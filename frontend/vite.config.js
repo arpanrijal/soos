bac181a8c5285c181c0b3bd44c4b92303285bc80
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-01-13 01:18:29 +0545

    add bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
-
+  console.log('Loaded VITE_BACKEND_URL:', env.VITE_BACKEND_URL);
   return defineConfig({
     plugins: [react()],
     preview: {
-      allowedHosts: [env.VITE_BACKEND_URL],
+      allowedHosts: [env.VITE_BACKEND_URL]
     },
   });
 };
