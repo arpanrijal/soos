a32671393c47959aeb3dc76d2fcd9785bd271918
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-01-13 15:16:12 +0545

    merging frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
-  
+
   return defineConfig({
     plugins: [react()],
     allowedHosts: [
       'soos-o0d5.onrender.com',
+      'soos-backend.onrender.com',
       'localhost'
     ],
   });
