d27b55ec7c52d0cd932507739c1b9088254f51a2
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-01-14 17:12:49 +0545

    fetching latest file

---
+++ backend/controller/img-uploader.js
@@ -0,0 +1,35 @@
+import FileDB from "../config/filedb.connect";
+
+(async function() {
+    
+    // Upload an image
+     const uploadResult = await FileDB.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = FileDB.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = FileDB.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log(autoCropUrl);    
+})();
