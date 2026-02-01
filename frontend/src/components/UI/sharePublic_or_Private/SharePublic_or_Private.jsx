358956cd7133ed062c0e75f5a01ef919d1c83f83
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-02-01 16:27:17 +0545

    added close button inside the share heading

--- frontend/src/components/UI/sharePublic_or_Private/SharePublic_or_Private.jsx
+++ frontend/src/components/UI/sharePublic_or_Private/SharePublic_or_Private.jsx
@@ -11,19 +11,19 @@ const SharePublic_or_Private = ({ shareLink, setisshareTabVisible }) => {
             settickbox(false)
         }, 600);
         setTimeout(() => {
-             clearInterval(ticktime);
+            clearInterval(ticktime);
         }, 700);
     }
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center px-3">
 
             <div className="absolute inset-0 bg-black/90"></div>
-            <div className='absolute right-5 top-8 cursor-pointer' onClick={() => {
-                setisshareTabVisible(false)
-            }}>
-                <X size={28} className="text-white" />
-            </div>
             <div className="relative z-10 flex rounded-3xl bg-white gap-5 h-fit p-4 flex-col min-w-7xl w-[550px]">
+                <div className='absolute right-5 top-3 cursor-pointer' onClick={() => {
+                    setisshareTabVisible(false)
+                }}>
+                    <X size={28} className="text-black" />
+                </div>
                 <h2 className="text-black font-semibold text-xl">Share Link</h2>
                 <div className='flex gap-5 flex-row justify-center items-center'>
                     <input
@@ -33,12 +33,12 @@ const SharePublic_or_Private = ({ shareLink, setisshareTabVisible }) => {
                         disabled
                         id='shareurl'
                     />
-                   {tickbox ? <div className="inline-block p-2 bg-green-200 rounded-lg cursor-pointer">
-                        <Check size={23} className='text-green-600'/>
+                    {tickbox ? <div className="inline-block p-2 bg-green-200 rounded-lg cursor-pointer">
+                        <div className='flex flex-row gap-1'><Check size={23} className='text-green-600' /><span>Copied</span></div>
                     </div> : <div className="inline-block p-2 bg-slate-200 rounded-lg cursor-pointer" onClick={() => {
                         copytextFunction()
                     }}>
-                        <Copy size={23} />
+                        <div className='flex flex-row gap-1'><Copy size={23} /><span>Copy</span></div>
                     </div>}
                 </div>
                 <div className="flex gap-2 text-lg text-black justify-start items-center">
